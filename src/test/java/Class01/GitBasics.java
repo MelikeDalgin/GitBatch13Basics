763efe3e4b6609c5c5830b5afd66f1376136838c
melikedalgin <mlkedalgin@gmail.com>
2022-10-23 16:38:35 -0400

    first commit

---
+++ src/test/java/Class01/GitBasics.java
@@ -0,0 +1,9 @@
+package Class01;
+
+public class GitBasics {
+    public static void main (String[] args) {
+        // write down a code that checks a temperature from a website and then
+        // prints out that is it a beautiful day or not
+        System.out.println("today is a shitty day");
+    }
+}
